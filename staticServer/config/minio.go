b2cf35ec7c337155d5515be8c968713242fe3101
RajendranDinesh <dinesh2842004@gmail.com>
2024-08-06 12:37:44 +0530

    [STATIC] serve built files from minio

---
+++ staticServer/config/minio.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+var Minio *minio.Client
+
+func InitMinioConnection() {
+	endpoint, endpointExists := os.LookupEnv("MIO_HOST")
+	accessKey, accessKeyExists := os.LookupEnv("MIO_ACCESS_ID")
+	secretAccessKey, secretExists := os.LookupEnv("MIO_SECRET")
+	sslStatus, sslExists := os.LookupEnv("MIO_SSL")
+	bucket, bucketExists := os.LookupEnv("MIO_BUCKET")
+
+	if !endpointExists || !accessKeyExists || !secretExists || !sslExists || !bucketExists {
+		log.Fatalln("[MINIO] required env variable is missing.")
+	}
+
+	var convErr error
+	var useSSL bool
+	useSSL, convErr = strconv.ParseBool(sslStatus)
+	if convErr != nil {
+		log.Panicln(convErr)
+		log.Fatalln("[MINIO] SSL env probs.")
+	}
+
+	var conErr error
+	Minio, conErr = minio.New(endpoint,
+		&minio.Options{
+			Creds:  credentials.NewStaticV4(accessKey, secretAccessKey, ""),
+			Secure: useSSL,
+		})
+	if conErr != nil {
+		log.Println(conErr)
+		log.Fatalln("[MINIO] Connection probs.")
+	}
+
+	log.Println("[MINIO] Connected to ", Minio.EndpointURL().Host)
+
+	ctx := context.Background()
+
+	err := Minio.MakeBucket(ctx, bucket, minio.MakeBucketOptions{})
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, errBucketExists := Minio.BucketExists(ctx, bucket)
+		if errBucketExists == nil && exists {
+			log.Printf("[MINIO] Bucket %s already exists\n", bucket)
+		} else {
+			log.Fatalln("[MINIO] Couldn't create bucket ", err)
+		}
+	} else {
+		log.Printf("[MINIO] Successfully created %s\n", bucket)
+	}
+}
---
+++ staticServer/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"staticServer/config"
+	"strings"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+	"github.com/minio/minio-go/v7"
+)
+
+func init() {
+	initGoDotENV()
+	config.InitMinioConnection()
+}
+
+func getFile(fileName string) ([]byte, error) {
+	bucket, bucketExists := os.LookupEnv("MIO_BUCKET")
+	if !bucketExists {
+		return []byte(""), fmt.Errorf("[BUCKET] bucket name was not found in env")
+	}
+
+	object, getErr := config.Minio.GetObject(context.Background(), bucket, fileName, minio.GetObjectOptions{})
+	if getErr != nil {
+		fmt.Println(getErr)
+		return []byte(""), getErr
+	}
+
+	defer object.Close()
+
+	content, readErr := io.ReadAll(object)
+	if readErr != nil {
+		return []byte(""), readErr
+	}
+
+	return content, nil
+}
+
+func main() {
+	// Initialize a new Fiber app
+	app := fiber.New()
+
+	app.Get("*", func(c fiber.Ctx) error {
+		projectName := strings.Split(c.Hostname(), ".")[0]
+		fileName := projectName + c.Path()
+		if c.Path() == "/" || !strings.Contains(c.Path(), ".") {
+			fileName = projectName + "/index.html"
+		}
+
+		file, err := getFile(fileName)
+		if err != nil {
+			return c.Status(fiber.StatusNotFound).SendString("File not found")
+		}
+
+		// Set the appropriate content type based on the file extension
+		switch {
+		case strings.HasSuffix(fileName, ".html"):
+			c.Type("html")
+		case strings.HasSuffix(fileName, ".js"):
+			c.Set("Content-Type", "application/javascript")
+		case strings.HasSuffix(fileName, ".css"):
+			c.Type("css")
+		case strings.HasSuffix(fileName, ".json"):
+			c.Type("json")
+		case strings.HasSuffix(fileName, ".png"):
+			c.Type("png")
+		case strings.HasSuffix(fileName, ".jpg"), strings.HasSuffix(fileName, ".jpeg"):
+			c.Type("jpeg")
+		case strings.HasSuffix(fileName, ".gif"):
+			c.Type("gif")
+		case strings.HasSuffix(fileName, ".svg"):
+			c.Type("svg+xml")
+		default:
+			c.Type("text")
+		}
+
+		return c.Send(file)
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
+}
+
+func initGoDotENV() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalln("[SERVER] Error Loading .env file")
+	}
+}
